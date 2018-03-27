e781999f8ce589b84da94e1d4b559bdede4d0780
Jeremy Cantu <jcantu521@gmail.com>
2018-03-27 11:24:35 -0500

    Fixed importation of semantic css file, deleted stale files

--- src/index.js
+++ src/index.js
@@ -7,7 +7,6 @@ import ReactDOM from 'react-dom';
 
 import { HashRouter } from 'react-router-dom';
 
-import './styles/semantic-ui-css/semantic.min.css';
 import './styles/styles.css';
 
 import registerServiceWorker from './registerServiceWorker';
